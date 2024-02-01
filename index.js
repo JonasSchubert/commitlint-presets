8e93492d6463c4f37548e8c15abd46ff2bc2ae87
Jonas Schubert <jonas.schubert.1990@web.de>
2024-02-01 19:33:01 +0100

    fix(rules): set length of scope-min-length to 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   rules: {
     'scope-case': [2, 'always', 'lower-case'],
     'scope-empty': [2, 'never'],
-    'scope-min-length': [2, 'always', 3],
+    'scope-min-length': [2, 'always', 2],
     'subject-case': [1, 'always', 'lower-case'],
     'subject-min-length': [2, 'always', 8]
   }
